488a659c8e424ae870926b7acfcba2d8f1d2ba69
cmcuatto <tinacuatto@Christina-Cuattos-MacBook.local>
2015-10-20 20:25:48 -0400

    final BOOM

---
+++ style.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+$('.required').on('blur', function(e){
+  validateField(this);  
+});
+
+$('#apply-form-id').on('submit', function(event){
+    var nameValid = validateField('.name');
+    var emailValid = validateField('.email');
+    var passValid = validateField('.password');
+    if(nameValid && emailValid && passValid){
+      return true;
+    } else {
+      return false;
+    }
+ 
+});
+
+function validateField(field){
+    var inputValue = $(field).val();  
+    if(inputValue === ''){  
+        $(field).addClass('error');
+        return false;
+    }else {
+        $(field).removeClass('error');
+        return true;
+    }
+}
+$(".readmore").click(function(){
+
+	$("#toggleMore").slideDown();
+
+	$(".readless").show();
+
+	$(".readmore").hide();
+	
+	return false;
+});
+
+$(".readless").click(function(){
+
+	$("#toggleMore").slideUp(function(){
+		$(".readmore").show();
+	});
+	
+	$(".readless").hide();
+
+	return false;
+});
+
+ $('.slider').slick({
+  	autoplay: true,
+  	autoplaySpeed: 3000,
+  	arrows: true,
+});
+				
+
+});
